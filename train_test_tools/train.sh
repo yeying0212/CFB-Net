0147ee81c2960f4911602fd9acd29dae7deb9612
yeying0212 <168414779+yeying0212@users.noreply.github.com>
2026-07-13 17:07:48 +0800

    1

--- train_test_tools/train.sh
+++ train_test_tools/train.sh
@@ -1,7 +1,10 @@
-python ./train_test_tools/train.py --file_root S1G --max_steps 26520 26521 --max_steps 40000
+# Train CFB-Net on S1GFloods
+python ./train_test_tools/train.py --file_root S1G --lr 5e-4 --max_steps 26800
 
-python ./train_test_tools/train.py --file_root WHU --lr 5e-4 --max_steps 40000
+# Train CFB-Net on ETCI-2021
+python ./train_test_tools/train.py --file_root etci --lr 5e-4 --max_steps 100000
 
-python ./train_test_tools/train.py --file_root CDD --lr 5e-4 --max_steps 40000
+# Train CFB-Net on UrbanSARFloods
+python ./train_test_tools/train.py --file_root URBAN --lr 5e-4 --max_steps 30000
 
-python ./train_test_tools/train.py --file_root SYSU --lr 5e-4 --max_steps 40000
+
