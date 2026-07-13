7d95887741acd283a94393f165b6a61955e9df14
yeying0212 <168414779+yeying0212@users.noreply.github.com>
2026-07-13 17:07:19 +0800

    Update test.sh

--- train_test_tools/test.sh
+++ train_test_tools/test.sh
@@ -1,4 +1,10 @@
-python ./train_test_tools/test.py --file_root LEVIR --lr 5e-4 --max_steps 40000
-python ./train_test_tools/test.py --file_root WHU --lr 5e-4 --max_steps 40000
-python ./train_test_tools/test.py --file_root CDD --lr 5e-4 --max_steps 40000
-python ./train_test_tools/test.py --file_root SYSU --lr 5e-4 --max_steps 40000
+# Test CFB-Net on S1GFloods
+python ./train_test_tools/test.py --file_root S1G --lr 5e-4 --max_steps 26800
+
+# Test CFB-Net on ETCI-2021
+python ./train_test_tools/test.py --file_root etci --lr 5e-4 --max_steps 100000
+
+# Test CFB-Net on UrbanSARFloods
+python ./train_test_tools/test.py --file_root URBAN --lr 5e-4 --max_steps 30000
+
+
